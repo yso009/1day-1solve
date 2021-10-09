8c931468dc89868fdf77cc770b6c74fe85f3d7d8
yso009 <yso009@naver.com>
2021-10-09 18:10:30 +0900

    2021-10-09

---
+++ 12871-무한 문자열.py
@@ -0,0 +1,29 @@
+s = input()
+t = input()
+
+#최대공약수
+
+a, b = len(s), len(t)
+def dirtn(s,t):
+    global a,b
+    if a == 0 : 
+        return b
+    elif b == 0:
+        return a
+    if a >= b:
+        a = a % b
+    elif a <= b:
+        b = b % a
+    return dirtn(a,b)
+
+# print((a*b)//dirtn(a,b))
+def main(s,t): 
+    x = (a*b)//dirtn(a,b)
+    if s == t: return 1
+    else: 
+        s = s*(x//len(s))
+        t = t*(x//len(t))
+        if s == t:
+            return 1
+        else: return 0
+print(main(s,t))
--- 정렬/퀵정렬.py
+++ 정렬/퀵정렬.py
@@ -1,1 +1,9 @@
 # 시간 복잡도 O(nlog₂n)
+
+
+li = [8,5,2,6,9,3,1,4,0,7]
+
+pivot = li[0]
+
+for i in range(len(li)):
+    
