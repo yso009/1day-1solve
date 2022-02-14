473ce25a1abd448645405709e0997cc949809b79
yso009 <yso009@naver.com>
2022-02-14 16:55:20 +0900

    2022-02-14 삼각형 만들기 - 1448

---
+++ baekjoon/1448-삼각형 만들기.py
@@ -0,0 +1,19 @@
+n = int(input())
+li = [int(input()) for _ in range(n)]
+
+li.sort(reverse=True)
+
+check = True
+
+for i in range(len(li)-2):
+    if li[i] < li[i+1] + li[i+2]:
+        
+        result = li[i] + li[i+1] + li[i+2]
+        print(result)     
+        check = False
+        break
+
+if check == True: print(-1)
+
+
+
