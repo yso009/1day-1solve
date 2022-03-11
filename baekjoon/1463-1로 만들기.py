e1506d89ec8c8311ef07ab59561d58a0e88ca7af
yso009 <yso009@naver.com>
2022-03-11 17:45:30 +0900

    2022-03-11 1로 만들기 - 1463

---
+++ baekjoon/1463-1로 만들기.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+c = 0
+while True:
+    if n % 3 == 0:
+        n = n//3
+        c += 1
+    elif n % 2 == 0:
+        n = n//2
+        c += 1
+    elif n%3 !=0 and n%2 !=0:
+        n = n-1
+        c += 1
+    if n == 1:
+        break
+print(c)
