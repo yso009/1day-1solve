a5ef80618d815042e28a316e21362bc608f15fb9
yso009 <yso009@naver.com>
2022-03-27 00:14:36 +0900

    2022-03-27 히든 넘버 - 8595

---
+++ baekjoon/8595-히든넘버.py
@@ -0,0 +1,14 @@
+n = int(input())
+s = input()
+e='0'
+result = 0
+s +='p'
+for i in range(len(s)):
+  if s[i].isdigit() == True:
+    e += s[i]
+
+  else:
+    result+=int(e)
+    e = '0'
+
+print(result)
